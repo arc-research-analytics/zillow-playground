f15f9392f7bde4aecdba3e3de38beecc301493a8
Will Wright <willwright@Wills-MacBook-Pro-2.local>
2023-05-04 14:44:05 -0400

    updated files

--- zillow_vis.py
+++ zillow_vis.py
@@ -2,28 +2,26 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 import plotly.express as px
-# import conda
-import os
-
-conda_file_dir = conda.__file__
-conda_dir = conda_file_dir.split('lib')[0]
-proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
-os.environ["PROJ_LIB"] = proj_lib
-
 import geopandas as gpd
-
 
 # hide sidebar
 st.set_page_config(
     initial_sidebar_state="collapsed",
     layout="wide",
     )
+
 st.markdown(
     """
 <style>
     [data-testid="collapsedControl"] {
         display: none
     }
+    .reportview-container .main footer {visibility: hidden;}    
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    section.main > div:has(~ footer ) {
+    padding-bottom: 5px;}
+    div.block-container{padding-top:1.5rem;}
 </style>
 """,
     unsafe_allow_html=True,
