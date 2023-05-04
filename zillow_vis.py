761931a332ef05436900a927e1de7b5bdb19ed7f
Will Wright <willwright@Wills-MacBook-Pro-2.local>
2023-05-04 14:17:12 -0400

    updated files

--- zillow_vis.py
+++ zillow_vis.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import pandas as pd
-import geopandas as gpd
 import pydeck as pdk
 import plotly.express as px
+import conda
+import os
+
+conda_file_dir = conda.__file__
+conda_dir = conda_file_dir.split('lib')[0]
+proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
+os.environ["PROJ_LIB"] = proj_lib
+
+import geopandas as gpd
+
 
 # hide sidebar
 st.set_page_config(
@@ -20,13 +29,14 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# title
-st.title('Zillow Zestimate 30-Day Change')
+# # title
+# st.title('May 30-Day Zestimate Change')
 
 # create dropdown for summary level
 geography = st.radio(
-    'Select geography to visualize:',
-    ('County', 'Super district'))
+    'Select geography to summarize:',
+    ('County', 'Super district'),
+    index=1)
 
 
 
