3688cb03137263509ba2c30dc39bc2ea9e2c1ed2
Will Wright <willwright@Wills-MacBook-Pro-2.local>
2023-05-04 23:18:57 -0400

    updated files

--- zillow_vis.py
+++ zillow_vis.py
@@ -38,7 +38,7 @@ st.markdown(
 def data_loader():
 
     # super districts first
-    df_SD = pd.read_csv('superDistrict_final.csv')
+    df_SD = pd.read_csv('superDistrict_final2.csv')
 
     url = "https://services1.arcgis.com/Ug5xGQbHsD8zuZzM/arcgis/rest/services/ACS_2021_Population/FeatureServer/20/query?where=PlanningRegion%20%3D%20'ATLANTA%20REGIONAL%20COMMISSION'&outFields=NAME,GEOID&outSR=4326&f=json"
 
@@ -49,7 +49,7 @@ def data_loader():
 
 
     # counties next
-    df_county = pd.read_csv('county_final.csv')
+    df_county = pd.read_csv('county_final2.csv')
 
     url = "https://services1.arcgis.com/Ug5xGQbHsD8zuZzM/arcgis/rest/services/ACS_2021_Population/FeatureServer/9/query?where=PlanningRegion%20%3D%20'ATLANTA%20REGIONAL%20COMMISSION'&outFields=GEOID,NAME&outSR=4326&f=json"
 
@@ -79,8 +79,13 @@ def superDistrict_mapper():
         (177, 0, 38) # dark red
     ]
 
+    var_dict = {
+        'Current Median Home Value':gdf['zestimate_median'],
+        '30-Day Change':gdf['change_median']
+    }
+
     gdf['choro_color'] = pd.cut(
-            gdf['Value_median'],
+            var_dict[variable],
             bins=len(color_labels),
             labels=color_labels,
             include_lowest=True,
@@ -95,7 +100,7 @@ def superDistrict_mapper():
         min_zoom=8,
         pitch=0,
         bearing=0,
-        height=590
+        height=550
     )
 
     geojson = pdk.Layer(
@@ -147,7 +152,7 @@ def county_mapper():
         min_zoom=8,
         pitch=0,
         bearing=0,
-        height=590
+        height=550
     )
 
     geojson = pdk.Layer(
@@ -188,6 +193,8 @@ variable = col2.radio(
     ('Current Median Home Value', '30-Day Change'),
     index=1,
     horizontal=True)
+
+
 
 
 # show map
